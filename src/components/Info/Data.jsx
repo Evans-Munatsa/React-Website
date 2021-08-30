217c88f39996273ed4938e4386193bf9c69ece92
Evans-Munatsa <evansmunatsa7@gmail.com>
2021-08-30 13:34:42 +0200

    react website

---
+++ src/components/Info/Data.jsx
@@ -0,0 +1,49 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lighttextDesc: true,
+  topLine: 'Secure Website',
+  headline: 'Security for our Websites and WebApps',
+  description: 'Security is one of the biggest thing in the tech industry and we strive to give the best security for our Websites and WebApps.',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: '../../images/svg-3.svg',
+  alt: 'car',
+  dark: true,
+  primary: true,
+  darkText: false
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lighttextDesc: false,
+  topLine: 'Unlimited Support',
+  headline: '24/7 Tech Support To Our Clients',
+  description: 'We are available 24/7 to support our Clients so they will not feel left out or beign put on a standby',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: '/images/svg-3.svg',
+  alt: 'Piggybanck',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lighttextDesc: false,
+  topLine: 'Premium Bank',
+  headline: 'Creating an account is extremely easy',
+  description: 'Get access to our exclusive app that allows you to send unlimited transactions whitout getting charged any fees.',
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: '/images/svg-3.svg',
+  alt: 'car',
+  primary: false,
+  darkText: true
+};
